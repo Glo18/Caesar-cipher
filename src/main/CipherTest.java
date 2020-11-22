523a61ee7c60e78d26ba7df83aaceaae852f2fcb
Glo18 <kiogoraspecial@gmail.com>
2020-11-22 20:37:54 +0300

    write a test

---
+++ src/main/CipherTest.java
@@ -0,0 +1,14 @@
+package main;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class CipherTest {
+@Test
+    public void
+    newCipher_leftshift3placesdownthea;() {
+    Cipher testCipher = new Cipher();
+    expectedOutput.substitute(D, A);
+    assertEquals(expectedOutput,testCipher.newCipher);
+}
+}
