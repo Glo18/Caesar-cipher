485f5b2e4c25b7f08a1702fac86f262936dfc4e0
Glo18 <kiogoraspecial@gmail.com>
2020-11-23 21:08:15 +0300

    add encryption key

--- src/main/App.java
+++ src/main/App.java
@@ -1,5 +1,7 @@
 package main;
 
+import models.Cipher;
+
 import java.util.Scanner;
 
 public class App {
@@ -11,5 +13,26 @@ public class App {
 
         System.out.println("Welcome! Would you like to encrypt or decrypt a statement?");
         String userEncryption = myConsole.readLine();
+
+        if (userEncryption.equals("encrypt")) {
+            System.out.println("Enter your statement:");
+            String statement = myConsole.readLine();
+
+            System.out.println("Enter encryption key:");
+            int key = myScanner.nextInt();
+
+            Cipher userEncryption = new Cipher(statement, key);
+            String encryption = userEncryption.isEncrypted(statement, key);
+            System.out.println("The statement that's encrypted is /n" + encryption);
+        }
+        else if (userEncryption.equals("decrypt")) {
+            System.out.println("Enter your statement:");
+            String statementDecrypt = myConsole.readLine();
+
+            System.out.println("Enter decrption key:");
+            int key = myScanner.nextInt();
+
+            C
+        }
     }
 }
