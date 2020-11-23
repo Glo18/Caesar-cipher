73507343e459f574b346835707cf22a68e3dda42
Glo18 <kiogoraspecial@gmail.com>
2020-11-23 20:02:03 +0300

    user input

--- src/main/App.java
+++ src/main/App.java
@@ -1,4 +1,15 @@
 package main;
 
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args) {
+        console myConsole = System.console();
+        Scanner myScanner = new Scanner(System.in);
+
+        boolean runProgram = true;
+
+        System.out.println("Welcome! Would you like to encrypt or decrypt a statement?");
+        String userEncryption = myConsole.readLine();
+    }
 }
